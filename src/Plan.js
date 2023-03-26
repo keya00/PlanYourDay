03ec14603a1190076cfea3ff06e911f7be27bc4f
Keya Khandelwal <you@example.com>
2023-03-26 15:28:19 +0530

    first commit

--- src/Plan.js
+++ src/Plan.js
@@ -1,9 +1,18 @@
-function Plan(props){
-    return<>
-    <li className="shadow p-d my-3 col-sm-9">{props.value}</li>
-    <button className="btn btn-danger my-2 col-sm-2 offset-1" onClick={()=>{props.sendData(props.id)}}>REMOVE</button>
-    <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="..."></input>
+function Plan(props) {
+  return (
+    <>
+      <li className="shadow p-d my-3 col-sm-9">{props.value}</li>
+      <button
+        className="btn btn-danger my-2 col-sm-2 offset-1"
+        onClick={() => {
+          props.sendData(props.id);
+        }}
+      >
+        REMOVE
+      </button>
+      {/* <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="..."></input> */}
     </>
+  );
 }
 
 export default Plan;
